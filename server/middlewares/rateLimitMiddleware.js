e4a3900e202f51845560e2e705fdaf6cb4ba9ee8
Samrath <samrathreddy04@gmail.com>
2025-01-01 18:15:21 +0530

    Trying to fix after Host few issue

--- server/middlewares/rateLimitMiddleware.js
+++ server/middlewares/rateLimitMiddleware.js
@@ -3,6 +3,7 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 40,
+  trustProxy: true,
   handler: (req, res) => {
     res.status(429).json({
       message: "Too many requests, please try again after 15 minutes."
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 app.use(express.static(path.resolve(__dirname,'build')))
 
